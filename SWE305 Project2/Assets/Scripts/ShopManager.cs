d024411c972ef886f34572661b090484f31efd2b
Bryan Cheam <117786629+Lubixz@users.noreply.github.com>
2022-12-22 02:49:43 +0800

    pok

--- SWE305 Project2/Assets/Scripts/ShopManager.cs
+++ SWE305 Project2/Assets/Scripts/ShopManager.cs
@@ -21,20 +21,16 @@ public class ShopManager : MonoBehaviour
         //ID
         shopItems[1, 1] = 1;
         shopItems[1, 2] = 2;
-        shopItems[1, 3] = 3;
-        shopItems[1, 4] = 4;
+
 
         //Prices = equals the value
         shopItems[2, 1] = 10;
         shopItems[2, 2] = 20;
-        shopItems[2, 3] = 30;
-        shopItems[2, 4] = 40;
 
         //Quantity, 0 cause havent buy anything
         shopItems[3, 1] = 0;
         shopItems[3, 2] = 0;
-        shopItems[3, 3] = 0;
-        shopItems[3, 4] = 0;
+ 
 
 
     }
